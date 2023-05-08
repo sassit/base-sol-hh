cf3969454ae722f859960ee24043a279e7b538a4
Tarik Sassi <tarik.sassi@gmail.com>
2023-05-09 00:04:09 +0200

    Interim fix, ts-node does not initiate hre, and verifiy needs the network parameter. So hh was used instead.

--- scripts/Ballot.ts
+++ scripts/Ballot.ts
@@ -1,18 +1,19 @@
-import { run, ethers } from "hardhat"
+import hre from "hardhat"
 import * as dotenv from "dotenv"
 import { Ballot__factory } from "../typechain-types"
 import { formatBytes32String } from "ethers/lib/utils"
+
 dotenv.config()
 
 const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"].map(formatBytes32String)
 
 async function main() {
-    await run("compile")
+    await hre.run("compile")
     console.log(`Private Key: ${process.env.PRIVATE_KEY}`)
-    console.log(`Network: ${process.env.NETWORK}`)
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "")
+    const wallet = new hre.ethers.Wallet(process.env.PRIVATE_KEY ?? "")
     console.log(`Wallet Address: ${wallet.address}`)
-    const provider = ethers.providers.getDefaultProvider("sepolia")
+    const provider = hre.ethers.providers.getDefaultProvider(process.env.ETH_SEPOLIA_URL)
+    console.log(`Network name: ${(await provider.getNetwork()).name}`)
     const latest = await provider.getBlock("latest")
     console.log(`Latest block: ${latest.number}`)
     const signer = wallet.connect(provider)
@@ -25,7 +26,7 @@ async function main() {
     console.log(`The ballot contract was deployed in transaction ${deployTx.blockHash}`)
     console.log(`The ballot contract was deployed at ${ballotContract.address}`)
     console.log(`The ballot contract is verifiied with api key ${process.env.ETHERSCAN_API_KEY}`) 
-    await run(`verify:verify`, {
+    await hre.run(`verify:verify`, {
         address: ballotContract.address,
         constructorArguments: [PROPOSALS],
       });
